5b5bf7cd3c4982007cc1fb46ec5e7da8154f6b27
Your Name <you@example.com>
2016-09-22 10:48:01 +0100

    added Multiple project

--- Multiple ex/ViewController.swift
+++ Multiple ex/ViewController.swift
@@ -10,6 +10,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //variables
+    var num = 0
+    var currentMultiple = 0
+    var times = 0
+    
+    
+    //Outlets
+    
+    @IBOutlet weak var multipleImg: UIImageView!
+   
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBOutlet weak var askerTxt: UITextField!
+    
+    @IBOutlet weak var pressLabel: UILabel!
+    
+    @IBOutlet weak var addBtn: UIButton!
+  
+   //Actions
+    
+    @IBAction func whenPlay(sender: UIButton!) {
+    
+    if askerTxt.text != nil && askerTxt.text != "" {
+        num = Int(askerTxt.text!)!
+        changeHiding()
+        
+        currentMultiple = num
+        pressLabel.text = "0 + \(num) = \(currentMultiple)"
+        times = 0
+    }
+    }
+    
+    @IBAction func addPressed(sender: UIButton!) {
+        
+        if times < 10 {
+            let oldMultiple = currentMultiple
+            currentMultiple = currentMultiple + num
+        pressLabel.text = "\(oldMultiple) + \(num)= \(currentMultiple)"
+            times++
+            
+            NSLog("\(times)")
+        }else {
+            changeHiding()
+            askerTxt.text = ""
+        }
+        
+    }
+    
+    
+    func changeHiding()
+    {
+        multipleImg.hidden = !multipleImg.hidden
+        playBtn.hidden = !playBtn.hidden
+        askerTxt.hidden = !askerTxt.hidden
+        
+        pressLabel.hidden = !pressLabel.hidden
+        addBtn.hidden = !addBtn.hidden
+        
+    }
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
